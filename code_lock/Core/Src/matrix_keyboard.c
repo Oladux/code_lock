9c3664615373cfa0a3f3fdb4dedd4c620f297b94
Oladux <stepasemin@mail.ru>
2024-05-02 19:53:36 +0300

    Adding commas

--- code_lock/Core/Src/matrix_keyboard.c
+++ code_lock/Core/Src/matrix_keyboard.c
@@ -6,7 +6,9 @@
  */
 
 #include "matrix_keyboard.h"
+
 static uint8_t input_buff[4]={0};
+
 void out_pin_change(uint8_t pin){
 	switch (pin) {
 			case 0:
@@ -14,25 +16,25 @@ void out_pin_change(uint8_t pin){
 				HAL_GPIO_WritePin(OUT_PIN_1_PORT, OUT_PIN_1_NUM, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(OUT_PIN_2_PORT, OUT_PIN_2_NUM, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(OUT_PIN_3_PORT, OUT_PIN_3_NUM, GPIO_PIN_RESET);
-						break;
+				break;
 			case 1:
 				HAL_GPIO_WritePin(OUT_PIN_0_PORT, OUT_PIN_0_NUM, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(OUT_PIN_1_PORT, OUT_PIN_1_NUM, GPIO_PIN_SET);
 				HAL_GPIO_WritePin(OUT_PIN_2_PORT, OUT_PIN_2_NUM, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(OUT_PIN_3_PORT, OUT_PIN_3_NUM, GPIO_PIN_RESET);
-						break;
+				break;
 			case 2:
 				HAL_GPIO_WritePin(OUT_PIN_0_PORT, OUT_PIN_0_NUM, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(OUT_PIN_1_PORT, OUT_PIN_1_NUM, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(OUT_PIN_2_PORT, OUT_PIN_2_NUM, GPIO_PIN_SET);
 				HAL_GPIO_WritePin(OUT_PIN_3_PORT, OUT_PIN_3_NUM, GPIO_PIN_RESET);
-						break;
+				break;
 			case 3:
 				HAL_GPIO_WritePin(OUT_PIN_0_PORT, OUT_PIN_0_NUM, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(OUT_PIN_1_PORT, OUT_PIN_1_NUM, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(OUT_PIN_2_PORT, OUT_PIN_2_NUM, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(OUT_PIN_3_PORT, OUT_PIN_3_NUM, GPIO_PIN_SET);
-						break;
+				break;
 		}
 }
 uint8_t* input_scan(){
